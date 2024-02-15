cf675284878801d351e06db6adeac9171291d9fb
ssolson <sterlingolson@yahoo.com>
2024-02-15 14:46:22 -0700

    use ep data

--- app/api/episode/route.ts
+++ app/api/episode/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-// import { getClientAndDb } from "../mongo/db";
 import getEpisodeData from "../../utils/dbUtils";
 
 export const dynamic = "force-dynamic";
