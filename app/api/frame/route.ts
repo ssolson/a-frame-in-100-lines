4d06b361a4e1e4b1268dd4473814cf44ff8ce014
ssolson <sterlingolson@yahoo.com>
2024-02-14 17:15:35 -0700

    use og route

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -27,7 +27,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         },
       ],
       image: {
-        src: `${NEXT_PUBLIC_URL}/park-1.png`,
+        src: ogImageUrl,
       },
       postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
     }),
