0276e1886a0e5a4d2740eac57f0426ef8fd0f43a
ssolson <sterlingolson@yahoo.com>
2024-02-15 14:50:35 -0700

    include ep num in request

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 import { NEXT_PUBLIC_URL } from './config';
 
+// Encode the dynamic text for safe URL inclusion
+const dynamicText = encodeURIComponent('730');
+
+// Construct the URL for the OG image with the dynamic text
+if (!process.env.NEXT_PUBLIC_URL) {
+  throw new Error('Invalid/Missing environment variable: "NEXT_PUBLIC_URL"');
+}
+
+const ogImageUrl = `${process.env.NEXT_PUBLIC_URL}/api/og?episode_number=${dynamicText}`;
+
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
@@ -9,7 +19,7 @@ const frameMetadata = getFrameMetadata({
     },
   ],
   image: {
-    src: `${process.env.NEXT_PUBLIC_URL}/api/og`,
+    src: ogImageUrl,
     // src: `${NEXT_PUBLIC_URL}/park-3.png`,
     aspectRatio: '1:1',
   },
