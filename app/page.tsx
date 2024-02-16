7f0240ff29b0a8cd8f2d63495a7ef4076a5e0929
ssolson <sterlingolson@yahoo.com>
2024-02-16 14:05:23 -0700

    comments

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 
 import { metadata } from './pageMetadata';
 
+// This is the Frame
 export { metadata };
 
+// This is the normal Page
 export default function Page() {
   return (
-    <div style={{ color: 'white', backgroundColor: 'black', height: '100vh', width: '100vw' }}>
+    <div style={{ color: 'white', backgroundColor: 'black' }}>
       <h1>TLDL Frames</h1>
     </div>
   );
