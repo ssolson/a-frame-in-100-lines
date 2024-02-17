c278dc2eebf550681c03f1d78ddfee536137a336
ssolson <sterlingolson@yahoo.com>
2024-02-17 11:31:45 -0700

    use getSegmentImageURL

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -9,11 +9,15 @@ import { segmentButtons } from '../../components/frame/buttons';
 import { frameInput } from '../../components/frame/input';
 import { framePostUrl } from '../../components/frame/postURL';
 
+import { getSegmentImageUrl } from '../../components/frame/image';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   // Get Episode Number
   const { searchParams } = new URL(req.url);
   const episodeNumberStr = searchParams.get('episode_number');
+  if (!episodeNumberStr) {
+    return NextResponse.json({ status: 400, message: 'Episode number not found' });
+  }
 
   // Validate Frame & get message (segment number)
   const body: FrameRequest = await req.json();
@@ -21,9 +25,11 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   const segmentNumber: string | undefined = getMessageInput(message);
 
   // Set the segment image URL
-  const segmentImageUrl = { 
-    src: `${process.env.NEXT_PUBLIC_URL}/api/segment?episode_number=${episodeNumberStr}&segment_number=${segmentNumber}`
-  };
+  // const segmentImageUrl = { 
+  //   src: `${process.env.NEXT_PUBLIC_URL}/api/segment?episode_number=${episodeNumberStr}&segment_number=${segmentNumber}`
+  // };
+
+  const segmentImageUrl = getSegmentImageUrl('segment', episodeNumberStr, segmentNumber)
 
   return new NextResponse(
     getFrameHtmlResponse({
