683c2759da627d8b3a9bef0626492d2c230f470a
ssolson <sterlingolson@yahoo.com>
2024-02-17 20:15:47 -0700

    also export constant metadata for old method

--- app/components/pageMetadata.ts
+++ app/components/pageMetadata.ts
@@ -3,6 +3,27 @@
 import type { Metadata } from 'next';
 import { frameMetadata } from './frame/frameMetadata';
 
+const title = 'TLDL Frames';
+const description = "Too Long Didn't Listen Frames";
+
+export const metadata: Metadata = {
+  //  Metadata for the page
+  title: title,
+  description: description,
+
+  // Standard Open Graph Metadata (Shows in Discord ect)
+  openGraph: {
+    title: title,
+    description: description,
+    images: [`${process.env.NEXT_PUBLIC_URL}/TLDL_wide.png`],
+  },
+
+  // Frames Metadata (Shows in Warpcast Frames)
+  other: {
+    ...frameMetadata,
+  },
+};
+
 export const getDynamicMetadata = (episodeNumber: string): Metadata => {
   const title = `TLDL Frames - Episode ${episodeNumber}`;
   const description = `Too Long Didn't Listen - Episode ${episodeNumber} Frames`;
