cdb7b5b71f9551bd10ee78eda8da4a013102f5f3
ssolson <sterlingolson@yahoo.com>
2024-02-17 20:13:41 -0700

    dynamic ep_num page

--- app/[episode_number]/page.tsx
+++ app/[episode_number]/page.tsx
@@ -1,14 +1,23 @@
 // app/[episode_number]/page.tsx
 
 import { useParams } from 'next/navigation';
-import { metadata } from '../components/pageMetadata';
+import { getDynamicMetadata } from '../components/pageMetadata';
 
-// This is the Frame
-export { metadata };
-
-// This is the normal Page
 export default function Page() {
-  const { episode_number } = useParams();
+  const params = useParams();
+  // Ensure `episode_number` is treated as a string, even if useParams() returns a string array
+  const episode_number = Array.isArray(params.episode_number)
+    ? params.episode_number[0]
+    : params.episode_number;
+
+  if (!episode_number) {
+    return <div>Episode number is required.</div>;
+  }
+
+  // This is the Frame
+  const metadata = getDynamicMetadata(episode_number);
+
+  // This is the normal Page
   return (
     <div style={{ color: 'white', backgroundColor: 'black' }}>
       <h1>TLDL Frames</h1>
--- app/components/pageMetadata.ts
+++ app/components/pageMetadata.ts
@@ -3,24 +3,25 @@
 import type { Metadata } from 'next';
 import { frameMetadata } from './frame/frameMetadata';
 
+export const getDynamicMetadata = (episodeNumber: string): Metadata => {
+  const title = `TLDL Frames - Episode ${episodeNumber}`;
+  const description = `Too Long Didn't Listen - Episode ${episodeNumber} Frames`;
 
-const title = 'TLDL Frames';
-const description = 'Too Long Didn\'t Listen Frames'
-
-export const metadata: Metadata = {
-  //  Metadata for the page
-  title: title,
-  description: description,
-
-  // Standard Open Graph Metadata (Shows in Discord ect)
-  openGraph: {
+  return {
+    // Dynamic Metadata for the page
     title: title,
     description: description,
-    images: [`${process.env.NEXT_PUBLIC_URL}/TLDL_wide.png`],
-  },
 
-  // Frames Metadata (Shows in Warpcast Frames) 
-  other: {
-    ...frameMetadata,
-  },
+    // Standard Open Graph Metadata (Shows in Discord, etc.)
+    openGraph: {
+      title: title,
+      description: description,
+      images: [`${process.env.NEXT_PUBLIC_URL}/TLDL_wide.png`],
+    },
+
+    // Frames Metadata (Shows in Warpcast Frames)
+    other: {
+      ...frameMetadata,
+    },
+  };
 };
