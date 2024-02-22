6cc00d7c5bb4d35c9e6aac9a80af0e55486c6aa0
ssolson <sterlingolson@yahoo.com>
2024-02-21 17:16:56 -0700

    add meta props manually

--- app/[episode_number]/page.tsx
+++ app/[episode_number]/page.tsx
@@ -1,28 +1,28 @@
 // app/[episode_number]/page.tsx
 
-import { GetServerSideProps } from 'next';
-import { useParams } from 'next/navigation';
 import { getDynamicMetadata } from '../components/pageMetadata';
-import { FrameMetadataType } from '@coinbase/onchainkit';
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { params } = context;
-  const episodeNumber = params?.episode_number;
-  // Ensure episodeNumber is a string
-  const episodeNumStr = typeof episodeNumber === 'string' ? episodeNumber : '700';
-
-  return {
-    props: {
-      metadata: getDynamicMetadata(episodeNumStr),
-    },
-  };
-};
+import { useParams } from 'next/navigation';
+import type { Metadata } from 'next';
 
 // This is the normal Page
-export default function Page({ metadata }: { metadata: FrameMetadataType }) {
+export default function Page() {
+  const { episode_number } = useParams<{ episode_number: string }>();
+
+  const metadata: Metadata = getDynamicMetadata(episode_number);
+
   return (
     <div style={{ color: 'white', backgroundColor: 'black' }}>
       <h1>TLDL Frames</h1>
+
+      <meta property="og:title" content={metadata?.openGraph?.title as string} />
+      <meta property="og:description" content={metadata?.openGraph?.description as string} />
+      <meta property="og:image" content={metadata?.openGraph?.images as string} />
+
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content={metadata?.other?.image as string} />
+      <meta property="fc:frame:input" content={metadata?.other?.input as string} />
+      <meta property="fc:frame:buttons" content={metadata?.other?.buttons as string} />
+      <meta property="fc:frame:postUrl" content={metadata?.other?.postUrl as string} />
     </div>
   );
 }
