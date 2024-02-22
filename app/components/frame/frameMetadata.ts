9f426a71dc02af63581fa436e59d91135c8eb95a
ssolson <sterlingolson@yahoo.com>
2024-02-21 20:23:25 -0700

    getDynamicFrameMetadata

--- app/components/frame/frameMetadata.ts
+++ app/components/frame/frameMetadata.ts
@@ -9,9 +9,18 @@ import { makeFramePostUrl } from './postURL';
 
 const episodeNumber: string = '730';
 
-export const frameMetadata = getFrameMetadata({   
+export const frameMetadata = getFrameMetadata({
   image: getTitleImageObject(episodeNumber),
   input: frameInput,
   buttons: titleButtons,
   postUrl: makeFramePostUrl(episodeNumber),
 });
+
+export const getDynamicFrameMetadata = (episodeNumber: string) => {
+  return getFrameMetadata({
+    image: getTitleImageObject(episodeNumber),
+    input: frameInput,
+    buttons: titleButtons,
+    postUrl: makeFramePostUrl(episodeNumber),
+  });
+};
