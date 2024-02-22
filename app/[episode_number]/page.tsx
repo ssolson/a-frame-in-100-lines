78cf968404a1a348178b6ef91f57b3ac3cc7954f
ssolson <sterlingolson@yahoo.com>
2024-02-21 17:51:56 -0700

    logs

--- app/[episode_number]/page.tsx
+++ app/[episode_number]/page.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from 'next';
 // This is the normal Page
 export default function Page({ params }: { params: { episode_number: string } }) {
   const metadata: Metadata = getDynamicMetadata(params.episode_number);
+  console.log(metadata);
 
   return (
     <div style={{ color: 'white', backgroundColor: 'black' }}>
@@ -18,7 +19,8 @@ export default function Page({ params }: { params: { episode_number: string } })
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content={metadata?.other?.image as string} />
       <meta property="fc:frame:input" content={metadata?.other?.input as string} />
-      <meta property="fc:frame:buttons" content={metadata?.other?.buttons as string} />
+      {/* <meta property="fc:frame:button" content={metadata?.other?.buttons[0] as string} /> */}
+      {/* <meta property="fc:frame:buttons" content={metadata?.other?.buttons as string} /> */}
       <meta property="fc:frame:postUrl" content={metadata?.other?.postUrl as string} />
     </div>
   );
